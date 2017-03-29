bc35e60334758717a5c18b6dfea69a879d2a7323
MARK ZIPETO <mzipeto@gmail.com>
2017-03-29 11:36:44 -0400

    Draft

---
+++ DWE 3/DWE 3 DRAFT.R
@@ -0,0 +1,24 @@
+# 0: Load the data in RStudio
+#Load the training and test data sets into RStudio, each in their own data frame.
+
+# 1: Merge data sets
+#Merge the training and the test sets to create one data set.
+
+# 2: Mean and standard deviation
+#Create two new columns, containing the mean and standard deviation for each measurement respectively. Hint: Since some feature/column names are repeated, you may need to use the make.names() function in R.
+
+# 3: Add new variables
+#Create variables called ActivityLabel and ActivityName that label all observations with the corresponding activity labels and names respectively
+
+#4: Create tidy data set
+#From the data set in step 3, creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+
+#5: Submit on github
+#Put all of your code for steps 1-4 above in a single R script file called run_analysis.R. In a single github repository, add the following:
+
+#  The tidy data set which is the result of steps 1-4
+#Your run_analysis.R script
+#A code book that describes the variables, the data, and any transformations or work that you performed to clean up the data called CodeBook.md.
+#A README.md file, that explains how all of the scripts work and how they are connected. 
+#Submit a link to your github repository containing all of these 4 deliverables.
